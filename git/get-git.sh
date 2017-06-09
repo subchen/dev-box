e8865463effe964599fab4683b64ed01cc550fd7
Guoqiang Chen <subchen@gmail.com>
2017-06-09 22:31:51 +0800

    Update get-git.sh

--- git/get-git.sh
+++ git/get-git.sh
@@ -25,7 +25,7 @@ function install_git_prompt() {
         ln -sf /etc/bash_completion.d/git-prompt /usr/local/bin/git-prompt.sh
     elif [ -f /usr/lib/git-core/git-sh-prompt ]; then
         # ubuntu
-        ln -sf/usr/lib/git-core/git-sh-prompt /usr/local/bin/git-prompt.sh
+        ln -sf /usr/lib/git-core/git-sh-prompt /usr/local/bin/git-prompt.sh
     elif [ -f /usr/local/etc/bash_completion.d/git-prompt.sh ]; then
         # mac brew
         ln -sf /usr/local/etc/bash_completion.d/git-prompt.sh /usr/local/bin/git-prompt.sh
