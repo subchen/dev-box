65d2239dfc488be597caf3d8710ee81135461878
Guoqiang Chen <subchen@gmail.com>
2017-06-09 22:30:28 +0800

    Update git-prompt.sh

--- git/get-git.sh
+++ git/get-git.sh
@@ -19,10 +19,27 @@ function install_git() {
     fi
 }
 
-install_git
+function install_git_prompt() {
+    if [ -f /etc/bash_completion.d/git-prompt ]; then
+        # ubuntu
+        ln -sf /etc/bash_completion.d/git-prompt /usr/local/bin/git-prompt.sh
+    elif [ -f /usr/lib/git-core/git-sh-prompt ]; then
+        # ubuntu
+        ln -sf/usr/lib/git-core/git-sh-prompt /usr/local/bin/git-prompt.sh
+    elif [ -f /usr/local/etc/bash_completion.d/git-prompt.sh ]; then
+        # mac brew
+        ln -sf /usr/local/etc/bash_completion.d/git-prompt.sh /usr/local/bin/git-prompt.sh
+    elif [ -f /usr/share/git-core/contrib/completion/git-prompt.sh ]; then
+        # centos
+        ln -sf /usr/share/git-core/contrib/completion/git-prompt.sh /usr/local/bin/git-prompt.sh
+    else
+        echo "Downloading git-prompt.sh ..."
+        proxy-sh curl -fSL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o /usr/local/bin/git-prompt.sh
+    fi
+}
 
-echo "Downloading git-prompt.sh ..."
-proxy-sh curl -fSL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o /usr/local/bin/git-prompt.sh
+install_git
+install_git_prompt
 
 echo "Configuring git ..."
 cp -rf $ROOT/root/.gitconfig       ~/
--- git/root/.bashrc.d/git.sh
+++ git/root/.bashrc.d/git.sh
@@ -1,7 +1,7 @@
-# git branch on bash prompt
-# https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-source /usr/local/bin/git-prompt.sh
+# git prompt on bash
+. /usr/local/bin/git-prompt.sh
 
 export GIT_PS1_SHOWDIRTYSTATE=1
 #export PS1='[\u@\h \w]$(__git_ps1)# '
 export PS1='[\[\e[1;34m\]\u@\h \[\e[1;32m\]\w\[\e[m\]]\[\e[1;35m\]$(__git_ps1)\[\e[m\]# '
+
