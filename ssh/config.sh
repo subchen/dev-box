91fd7b82dba30124b1a3274a85bae8650e82cff7
Guoqiang Chen <subchen@gmail.com>
2017-06-09 12:10:20 +0800

    Update ssh

--- ssh/config.sh
+++ ssh/config.sh
@@ -4,10 +4,13 @@ set -e
 
 ROOT=$(cd $(dirname $0); pwd)
 
-mkdir -p ~/.ssh
+mkdir -p -m 0700 ~/.ssh
 
 cp -f $ROOT/root/.ssh/* ~/.ssh/
 
-chmod 0700 ~/.ssh
+cp -f ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys
+
+chmod 0600 ~/.ssh/authorized_keys
 chmod 0600 ~/.ssh/id_rsa
 chmod 0644 ~/.ssh/id_rsa.pub
+
