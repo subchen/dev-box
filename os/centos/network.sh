86803cae01a96a3c348be7010c00c0e5d4add411
Guoqiang Chen <subchen@gmail.com>
2017-06-19 18:26:11 +0800

    Update centos network.sh

--- os/centos/network.sh
+++ os/centos/network.sh
@@ -2,14 +2,15 @@
 
 hostnamectl set-hostname devbox
 
-cat > /etc/sysconfig/network-scripts/ifcfg-eth160 << EOF
-DEVICE=eth160
+cat > /etc/sysconfig/network-scripts/ifcfg-eth0 << EOF
+DEVICE=eth0
 TYPE=Ethernet
 ONBOOT=yes
 BOOTPROTO=static
 IPADDR=192.168.10.11
 NETMASK=255.255.255.0
 GATEWAY=192.168.10.1
+IPV6INIT=no
 NM_CONTROLLED=no
 DEFROUTE=yes
 PEERDNS=no
@@ -22,7 +23,7 @@ cat > /etc/hosts << EOF
 127.0.0.1      localhost localhost.localdomain localhost4 localhost4.localdomain4
 ::1            localhost localhost.localdomain localhost6 localhost6.localdomain6
 
-192.168.10.11   devbox
+192.168.10.11  vbox
 EOF
 
 cat > /etc/resolv.conf << EOF
