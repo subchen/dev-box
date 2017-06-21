624babbe435311eb80dee4ef5316e87fe2267947
Guoqiang Chen <subchen@gmail.com>
2017-06-21 21:20:42 +0800

    Update bash config

--- bash/config.sh
+++ bash/config.sh
@@ -2,10 +2,7 @@
 
 ROOT=$(cd $(dirname $0); pwd)
 
-cp -f $ROOT/root/.bashrc.devbox ~/
-cp -f $ROOT/root/.tmux.conf     ~/
-
-mkdir -p ~/.bashrc.d/
+cp -f $ROOT/root/.* ~/
 
 echo -e "$(grep -v bashrc.devbox ~/.bashrc)\n\nsource ~/.bashrc.devbox\n" > ~/.bashrc
 source ~/.bashrc.devbox
