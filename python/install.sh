bec09de0469cf23ab852880ff7d056499f4440b5
Guoqiang Chen <subchen@gmail.com>
2017-06-26 18:23:04 +0800

    Update python

--- python/install.sh
+++ python/install.sh
@@ -2,7 +2,7 @@
 
 if ! type -P pip > /dev/null; then
     echo "Installing pip ..."
-    yum-default install -y python-devel
+    yum-default install -y python python-devel
     proxy-sh curl -fSL https://bootstrap.pypa.io/get-pip.py | proxy-sh python
 fi
 
