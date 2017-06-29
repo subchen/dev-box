561b38758b84705a8fa274234637b681c2d8a5c3
Guoqiang Chen <subchen@gmail.com>
2017-06-29 12:11:25 +0800

    Add vim plugins version

--- vim/update_vim_plugins.sh
+++ vim/update_vim_plugins.sh
@@ -3,32 +3,42 @@
 ROOT=$(cd $(dirname $0); pwd)
 
 BUNDLE_LIST="
-    VundleVim/Vundle.vim
-    scrooloose/nerdtree
+    VundleVim/Vundle.vim@v0.10.2
+    scrooloose/nerdtree@5.0.0
     Xuyuanp/nerdtree-git-plugin
-    ctrlpvim/ctrlp.vim
-    majutsushi/tagbar
-    mileszs/ack.vim
-    vim-scripts/ZoomWin
+    ctrlpvim/ctrlp.vim@1.80
+    majutsushi/tagbar@v2.7
+    mileszs/ack.vim@1.0.9
+    vim-scripts/ZoomWin@24
     flazz/vim-colorschemes
     chriskempson/base16-vim
-    vim-airline/vim-airline
+    vim-airline/vim-airline@v0.8
     vim-airline/vim-airline-themes
-    bronson/vim-trailing-whitespace
+    bronson/vim-trailing-whitespace@1.0
     mbriggs/mark.vim
-    editorconfig/editorconfig-vim
+    editorconfig/editorconfig-vim@v0.3.3
     tpope/vim-markdown
     elzr/vim-json
     vim-scripts/jsbeautify
-    fatih/vim-go
-    ervandew/supertab
+    fatih/vim-go@v1.13
+    ervandew/supertab@2.1
 "
 
 mkdir -p ~/.vim/bundle
 
-cd ~/.vim/bundle/
 for bundle in $BUNDLE_LIST; do
     echo "Installing vim bundle: $bundle ..."
-    git clone https://github.com/$bundle
+
+    IFS='@' bundle=($bundle)
+    name=${bundle[0]}
+    version=${bundle[1]}
+
+    cd ~/.vim/bundle/
+    git clone https://github.com/$name
+
+    if [ -n "$version" ]; then
+        cd ~/.vim/bundle/$(basename $name)
+        git reset --hard $version
+    fi
 done
 
