10942e1230480c112c0f01386ea20a6dae24488d
Guoqiang Chen <subchen@gmail.com>
2017-07-03 14:36:14 +0800

    Add findup

--- bash/config.sh
+++ bash/config.sh
@@ -2,6 +2,8 @@
 
 ROOT=$(cd $(dirname $0); pwd)
 
+cp -rf $ROOT/usr/local/bin/*      /usr/local/bin/
+
 cp -rf $ROOT/root/.bashrc.devbox  ~/
 cp -rf $ROOT/root/.bashrc.d/      ~/
 cp -rf $ROOT/root/.tmux.conf      ~/
