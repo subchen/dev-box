b67e93b30491b05b9e4802b6ee97e23d859de7df
Guoqiang Chen <subchen@gmail.com>
2018-01-12 11:25:20 +0800

    Create install-masos.sh

---
+++ powerline-go/install-masos.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+CWD=$(cd $(dirname $0); pwd)
+
+## binary
+# curl -fSL https://github.com/justjanne/powerline-go/releases/download/v1.8.2/powerline-go-darwin-amd64 \
+#    -o /usr/local/bin/powerline-go
+# chmod +x /usr/local/bin/powerline-go
+
+## using brew
+brew install subchen/tap/powerline-go
+
+## configure bash
+mkdir -p ~/.bash/
+cp -f $CWD/powerline-go.bash ~/.bash/
+echo ". ~/.bash/powerline-go.bash" >> ~/.bashrc
