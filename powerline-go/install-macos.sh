0fdebae34aa23de8d409cf32541e1691d2dd4714
Guoqiang Chen <subchen@gmail.com>
2018-01-12 11:27:44 +0800

    Update and rename install-masos.sh to install-macos.sh

---
+++ powerline-go/install-macos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+## binary
+# curl -fSL https://github.com/justjanne/powerline-go/releases/download/v1.8.2/powerline-go-darwin-amd64 -o /usr/local/bin/powerline-go
+# chmod +x /usr/local/bin/powerline-go
+
+## brew
+brew install subchen/tap/powerline-go
