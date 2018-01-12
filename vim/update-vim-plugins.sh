f2fb357f6b923ce0d96b7e4a019b835699ba99f7
Guoqiang Chen <subchen@gmail.com>
2018-01-12 22:26:41 +0800

    Update update-vim-plugins.sh

--- vim/update-vim-plugins.sh
+++ vim/update-vim-plugins.sh
@@ -5,21 +5,21 @@ CWD=$(cd $(dirname $0); pwd)
 BUNDLE_LIST="
     VundleVim/Vundle.vim@v0.10.2
     scrooloose/nerdtree@5.0.0
-    Xuyuanp/nerdtree-git-plugin
+    Xuyuanp/nerdtree-git-plugin@6a9b312
     ctrlpvim/ctrlp.vim@1.80
     majutsushi/tagbar@v2.7
     mileszs/ack.vim@1.0.9
     vim-scripts/ZoomWin@24
-    flazz/vim-colorschemes
-    chriskempson/base16-vim
+    flazz/vim-colorschemes@b8dff40
+    chriskempson/base16-vim@e5f2e53
     vim-airline/vim-airline@v0.8
-    vim-airline/vim-airline-themes
+    vim-airline/vim-airline-themes@43fe392
     bronson/vim-trailing-whitespace@1.0
-    mbriggs/mark.vim
+    mbriggs/mark.vim@abde906
     editorconfig/editorconfig-vim@v0.3.3
-    tpope/vim-markdown
-    elzr/vim-json
-    vim-scripts/jsbeautify
+    tpope/vim-markdown@a7dbc31
+    elzr/vim-json@f5e3181
+    vim-scripts/jsbeautify@0a18560
     fatih/vim-go@v1.13
     ervandew/supertab@2.1
 "
