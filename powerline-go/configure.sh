72927a202bdda488db73c86609e4ab44799ccc3d
Guoqiang Chen <subchen@gmail.com>
2018-01-12 11:37:23 +0800

    Update configure.sh

--- powerline-go/configure.sh
+++ powerline-go/configure.sh
@@ -3,6 +3,6 @@
 CWD=$(cd $(dirname $0); pwd)
 
 mkdir -p ~/.bash/
-cp -f $CWD/powerline-go.bash ~/.bash/
+cp -f $CWD/config/powerline-go.bash ~/.bash/
 
 echo ". ~/.bash/powerline-go.bash" >> ~/.bashrc
