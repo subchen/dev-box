56bc2188fdabf1397273b69a4efe0b011d49d086
Guoqiang Chen <subchen@gmail.com>
2018-01-12 14:46:10 +0800

    Update install.sh

--- python/install.sh
+++ python/install.sh
@@ -1,13 +1,9 @@
 #!/bin/bash -e
 
-if ! type -P pip > /dev/null; then
-    echo "Installing pip ..."
-    yum-default install -y python python-devel
-    proxy-sh curl -fSL https://bootstrap.pypa.io/get-pip.py | proxy-sh python
-fi
+yum install -y python python-devel
 
-if ! type -P virtualenv > /dev/null; then
-    echo "Installing virtualenv ..."
-    proxy-sh pip install virtualenv
-fi
+# pip
+curl -fSL https://bootstrap.pypa.io/get-pip.py | python
 
+# virtualenv
+pip install virtualenv
