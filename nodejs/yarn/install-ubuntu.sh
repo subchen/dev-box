9684942ab934d31f59da5ef70c5bcabc23d61f26
Guoqiang Chen <subchen@gmail.com>
2018-01-12 14:51:07 +0800

    Create install-ubuntu.sh

---
+++ nodejs/yarn/install-ubuntu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+apt-get update
+apt-get install yarn
