9081ad1c828ff8e0b834b147ef6d730930ece3a8
Guoqiang Chen <subchen@gmail.com>
2019-04-15 20:43:54 +0800

    Update install-centos.sh

--- vim/install-centos.sh
+++ vim/install-centos.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -e
 
-yum install -y vim ctags cscope
+yum install -y ctags cscope
+
+# vim 7.4
+yum install -y vim
+
+# vim 8.0
+curl -fSL https://copr.fedorainfracloud.org/coprs/lbiaggi/vim80-ligatures/repo/epel-7/lbiaggi-vim80-ligatures-epel-7.repo -o /etc/yum.repos.d/vim80.repo
+yum install -y vim-enhanced
