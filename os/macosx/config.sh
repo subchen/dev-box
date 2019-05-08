94e7ea71a6730b17a138ba03ed6a8942263b19cb
Guoqiang Chen <subchen@gmail.com>
2019-05-08 09:15:53 +0800

    Update config.sh

--- os/macosx/config.sh
+++ os/macosx/config.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -e
 
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+
+defaults write com.apple.TextEdit RichText -int 0
 
