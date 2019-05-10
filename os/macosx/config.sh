4d17db4873806c5cee5581529c23a11417cf96ad
Guoqiang Chen <subchen@gmail.com>
2019-05-10 11:42:30 +0800

    Update config.sh

--- os/macosx/config.sh
+++ os/macosx/config.sh
@@ -7,3 +7,7 @@ defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 
 defaults write com.apple.TextEdit RichText -int 0
 
+### refs ###
+# https://gist.github.com/vraravam/5e28ca1720c9dddacdc0e6db61e093fe
+# https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+
