5f87c2469515905f2b53d5518ce682aff53a69ee
Guoqiang Chen <subchen@gmail.com>
2019-05-10 11:38:01 +0800

    Update install-macos.sh

--- powerline-go/install-macos.sh
+++ powerline-go/install-macos.sh
@@ -2,4 +2,17 @@
 
 brew install subchen/tap/powerline-go
 
-brew cask install caskroom/fonts/font-consolas-for-powerline
+#brew cask install caskroom/fonts/font-consolas-for-powerline
+brew cask install caskroom/fonts/font-menlo-for-powerline
+
+
+
+/usr/libexec/PlistBuddy -c "Set:'New Bookmarks':0:'Use Non-ASCII Font' bool true" ~/Library/Preferences/com.googlecode.iterm2.plist
+/usr/libexec/PlistBuddy -c "Set:'New Bookmarks':0:'ASCII Anti Aliased' bool true" ~/Library/Preferences/com.googlecode.iterm2.plist
+/usr/libexec/PlistBuddy -c "Set:'New Bookmarks':0:'Non Ascii Font' string 'MenloForPowerline-Regular 12'" ~/Library/Preferences/com.googlecode.iterm2.plist
+
+
+/usr/libexec/PlistBuddy -c "Print:'New Bookmarks':0:'Use Non-ASCII Font'" ~/Library/Preferences/com.googlecode.iterm2.plist
+/usr/libexec/PlistBuddy -c "Print:'New Bookmarks':0:'ASCII Anti Aliased'" ~/Library/Preferences/com.googlecode.iterm2.plist
+/usr/libexec/PlistBuddy -c "Print:'New Bookmarks':0:'Non Ascii Font'" ~/Library/Preferences/com.googlecode.iterm2.plist
+
