734930f809913376233124ee5985a47ef409e045
Guoqiang Chen <subchen@gmail.com>
2019-11-21 23:40:54 +0800

    Update docker-compose.sh

--- app/docker/docker-compose.sh
+++ app/docker/docker-compose.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 
-curl -fSL https://github.com/docker/compose/releases/download/1.13.0/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose
+curl -fSL https://github.com/docker/compose/releases/download/1.13.0/docker-compose-$(uname -s)-$(uname -m) \
+     -o /usr/local/bin/docker-compose
+
 chmod +x /usr/local/bin/docker-compose
