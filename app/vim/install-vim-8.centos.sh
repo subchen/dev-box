506966d6f134577ea56c8c363aa74782c0e3fc84
Guoqiang Chen <subchen@gmail.com>
2019-11-21 23:32:24 +0800

    Update install-vim-8.centos.sh

--- app/vim/install-vim-8.centos.sh
+++ app/vim/install-vim-8.centos.sh
@@ -3,3 +3,6 @@
 # vim 8.0 for centos
 curl -fSL https://copr.fedorainfracloud.org/coprs/lbiaggi/vim80-ligatures/repo/epel-7/lbiaggi-vim80-ligatures-epel-7.repo -o /etc/yum.repos.d/vim80.repo
 yum install -y vim-enhanced
+
+# install ctags cscope
+yum install -y ctags cscope
