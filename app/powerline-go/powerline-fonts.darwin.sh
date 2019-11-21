632feefd3f628d5e6c6fe871d21865c463698ece
Guoqiang Chen <subchen@gmail.com>
2019-11-21 22:47:49 +0800

    Create powerline-fonts.darwin.sh

---
+++ app/powerline-go/powerline-fonts.darwin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+brew install caskroom/fonts/font-menlo-for-powerline
+#brew install caskroom/fonts/font-consolas-for-powerline
+
+# config iterm2
+iterm2_plist=$HOME/Library/Preferences/com.googlecode.iterm2.plist
+/usr/libexec/PlistBuddy -c "Set:'New Bookmarks':0:'Use Non-ASCII Font' bool true" $iterm2_plist
+/usr/libexec/PlistBuddy -c "Set:'New Bookmarks':0:'ASCII Anti Aliased' bool true" $iterm2_plist
+/usr/libexec/PlistBuddy -c "Set:'New Bookmarks':0:'Non Ascii Font' string 'MenloForPowerline-Regular 12'" $iterm2_plist
