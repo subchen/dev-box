dac1da00aa701023c6a99a077dda4aeaa1955e41
Guoqiang Chen <subchen@gmail.com>
2019-11-21 23:44:28 +0800

    Create centos.sh

---
+++ os/centos/centos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+echo "Installing RPMs ..."
+
+yum-default install -y \
+    vim tmux wget curl zip unzip which \
+    net-tools tree psmisc bind-utils nmap-ncat \
+    bash-completion \
+    git rsync \
+    yajl \
+    ntp \
+    yum-utils createrepo \
+    make automake autoconf libtool gcc gcc-c++ cmake \
+    epel-release
+
+yum-epel install -y \
+    sshpass the_silver_searcher jq
+
+yum-rpmforge install -y \
+    fuse-sshfs
