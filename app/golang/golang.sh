5908d2144c6fd4170c4fc9fca65ea1d9f70d2a62
Guoqiang Chen <subchen@gmail.com>
2019-11-21 23:08:51 +0800

    Create golang.sh

---
+++ app/golang/golang.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+OS=$(uname -s | tr [A-Z] [a-z])
+
+GOLANG_VERSION=1.13.4
+GOLANG_FILENAME=go${GOLANG_VERSION}.$OS-amd64.tar.gz
+GOLANG_HOME=/usr/local/go/${GOLANG_VERSION}
+
+echo "Downloading $GOLANG_FILENAME ..." \
+ && proxy-sh curl -fSL https://dl.google.com/go/${GOLANG_FILENAME}        -o ${GOLANG_FILENAME} \
+ && proxy-sh curl -fSL https://dl.google.com/go/${GOLANG_FILENAME}.sha256 -o ${GOLANG_FILENAME}.sha256 \
+ && echo "$(cat ${GOLANG_FILENAME}.sha256) $GOLANG_FILENAME" | sha256sum -c - \
+ && mkdir -p $GOLANG_HOME \
+ && tar -zxf $GOLANG_FILENAME -C $GOLANG_HOME --strip-components=1 \
+ && rm -f $GOLANG_FILENAME ${GOLANG_FILENAME}.sha256
+
+echo "Creating linking ..." \
+ && ln -sf $GOLANG_HOME/bin/go       /usr/local/bin/go \
+ && ln -sf $GOLANG_HOME/bin/gofmt    /usr/local/bin/gofmt \
+ && ln -sf $GOLANG_HOME/bin/godoc    /usr/local/bin/godoc
+
+
+mkdir -p ~/.bash
+cat > ~/.bash/golang.bash << EOF
+export GOROOT=$GOLANG_HOME
+export GOPATH=/go
+export PATH=\$GOPATH/bin:\$GOROOT/bin:\$PATH
+EOF
+echo ". ~/.bash/golang.bash" >> ~/.bashrc
