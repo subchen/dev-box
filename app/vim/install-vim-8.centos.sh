dfdadcfea8a04ebe86343e4a3be011f255174635
Guoqiang Chen <subchen@gmail.com>
2020-05-11 16:08:51 +0800

    Update install-vim-8.centos.sh

--- app/vim/install-vim-8.centos.sh
+++ app/vim/install-vim-8.centos.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-# vim 8.0 for centos
+# vim 8.0 for centos 7.x
+# centos 8.x can install by yum directly
 curl -fSL https://copr.fedorainfracloud.org/coprs/lbiaggi/vim80-ligatures/repo/epel-7/lbiaggi-vim80-ligatures-epel-7.repo -o /etc/yum.repos.d/vim80.repo
 yum install -y vim-enhanced
 
