660153f8ad12055148055a49fc0f200be0705041
Guoqiang Chen <subchen@gmail.com>
2020-05-27 17:27:29 +0800

    Update os-detect.sh

--- os-detect.sh
+++ os-detect.sh
@@ -2,18 +2,22 @@
 
 #
 # eval "$(./os-detect.sh)"
-# echo $OS_FAMILY
+# echo $OS_NAME
+#
+# OS_PLATFORM=darwin/linux
+# OS_NAME=darwin/centos/rhel/fedora/alpine/ubuntu/...
+# OS_VERSION=10.13.6/8/...
 #
 
-if [ $(uname -s) == "Darwin" ]; then
-    echo "OS_FAMILY=darwin"
+if [ "$(uname -s)" == "Darwin" ]; then
+    echo "OS_PLATFORM=darwin"
     echo "OS_NAME=darwin"
     echo "OS_VERSION=$(sw_vers -productVersion)"
 fi
 
-if [ $(uname -s) == "Linux" ]; then
+if [ "$(uname -s)" == "Linux" ]; then
     source /etc/os-release
-    echo "OS_FAMILY=linux"
+    echo "OS_PLATFORM=linux"
     echo "OS_NAME=$ID"
     echo "OS_VERSON=$VERSION_ID"
 fi
