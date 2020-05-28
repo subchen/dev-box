6691b22310f3d35f3069983e9be699ea72b9927e
Guoqiang Chen <subchen@gmail.com>
2020-05-28 23:33:17 +0800

    Update node.sh

--- app/node/node.sh
+++ app/node/node.sh
@@ -6,8 +6,8 @@ NODE_VERSION=12.16.3
 NODE_FILENAME=node-v$NODE_VERSION-$OS-x64.tar.gz
 
 echo "Downloading $NODE_FILENAME ..." \
- && proxy-sh curl -fSL https://nodejs.org/dist/v$NODE_VERSION/$NODE_FILENAME -o $NODE_FILENAME \
- && proxy-sh curl -fSL https://nodejs.org/dist/v$NODE_VERSION/SHASUMS256.txt -o SHASUMS256.txt \
+ && curl -fSL https://nodejs.org/dist/v$NODE_VERSION/$NODE_FILENAME -o $NODE_FILENAME \
+ && curl -fSL https://nodejs.org/dist/v$NODE_VERSION/SHASUMS256.txt -o SHASUMS256.txt \
  && grep $NODE_FILENAME SHASUMS256.txt | sha256sum -c \
  && tar -xzf $NODE_FILENAME -C /usr/local --strip-components=1 \
  && rm -f $NODE_FILENAME SHASUMS256.txt
