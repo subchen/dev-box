5d9f73860311acce945d47ef96609f9b5aeb62c4
Guoqiang Chen <subchen@gmail.com>
2020-08-07 10:45:52 +0800

    Update git-bash-completion.sh

--- app/git/git-bash-completion.sh
+++ app/git/git-bash-completion.sh
@@ -5,6 +5,9 @@ GIT_VERSION=master
 
 curl -fSL https://raw.githubusercontent.com/git/git/$GIT_VERSION/contrib/completion/git-completion.bash \
      -o /usr/local/opt/bash-completion/etc/bash_completion.d/git-completion.bash
+     
+# /usr/local/etc/bash_completion.d/git-completion.bash
+# /usr/local/opt/bash-completion/etc/bash_completion.d/git-completion.bash
 
 #brew unlink bash-completion
 #brew link bash-completion
