5e2db358d71025da423953661fa687a4d0169c51
QC <subchen@gmail.com>
2023-09-27 13:37:37 +0800

    Create install-apps.bat

---
+++ os/windows/install-apps.bat
@@ -0,0 +1,25 @@
+// powershell
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+// offical install
+irm get.scoop.sh | iex
+
+// 国内镜像 https://github.com/lzwme/scoop-proxy-cn
+irm https://ghproxy.com/raw.githubusercontent.com/lzwme/scoop-proxy-cn/master/install.ps1 | iex
+
+
+// scope 各种软件目录
+// https://rasa.github.io/scoop-directory/search
+
+// cmd
+scoop bucket add extras
+scoop bucket add java
+scoop bucket add scoopcn https://github.com/scoopcn/scoopcn.git
+
+
+scoop install aria2
+
+scoop install curl openssl git 
+
+
+scoop install openjdk8 openjdk17 maven
