31c731245613444398ddee8c7fb714f5d6c6c3ea
QC <subchen@gmail.com>
2023-12-26 11:57:10 +0800

    Update bash-completion.bash

--- .bash/bash-completion.bash
+++ .bash/bash-completion.bash
@@ -3,7 +3,9 @@
 if [ -f /usr/share/bash-completion/bash_completion ]; then
     . /usr/share/bash-completion/bash_completion # linux
 elif [ -f /usr/local/etc/bash_completion ]; then
-    . /usr/local/etc/bash_completion # macosx, brew
+    . /usr/local/etc/bash_completion # macosx, brew (Intel)
+elif [ -f /opt/homebrew/etc/bash_completion ]; then
+    . /opt/homebrew/etc/bash_completion # macosx, brew (Apple Silicon)
 elif [ -f /etc/bash_completion ]; then
     . /etc/bash_completion # legacy
 fi
